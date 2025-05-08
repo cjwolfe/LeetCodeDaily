3c6dbf54ebec33786719a3ad0b523f5afd98c38d
Chris <65196986+cjwolfe@users.noreply.github.com>
2025-05-08 15:00:58 -0400

    2025 05 08

---
+++ 2. Hashing/CountingElements/CountingElements.java
@@ -0,0 +1,19 @@
+
+public class CountingElements{
+    public static int countElements(int[] arr){
+    
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3};
+        int[] arr2 = {1,1,3,3,5,5,7,7};
+
+        System.out.println("Ans is " + countElements(arr));
+        System.out.println("Ans2 is " + countElements(arr2));
+
+
+
+    }
+
+}
