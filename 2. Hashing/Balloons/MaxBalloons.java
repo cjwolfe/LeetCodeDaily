b18078a1a13010341e3575dc4a96bfcafe9f9734
Chris <65196986+cjwolfe@users.noreply.github.com>
2025-05-13 17:05:27 -0400

    2025 05 13 v1

--- 2. Hashing/Balloons/MaxBalloons.java
+++ 2. Hashing/Balloons/MaxBalloons.java
@@ -1,22 +1,29 @@
+
 import java.util.*;
 public class MaxBalloons {
-    public static int maxNumberOfBalloons(String text) {
-        String ref = "balloon";
+
+    public String ref = "balloon";
+    public char[] refArray = ref.toCharArray();
+
+    public int maxNumberOfBalloons(String text) {
+        
 
         if(text.length() < ref.length()) return 0;
-
-        char[] refArray = ref.toCharArray();
 
         //convert text to array
         char[] input = text.toCharArray();
         
-
         //compare using hashmap?
         HashMap<Character, Integer> map = new HashMap<>();
         for (char chars : refArray){
-            if(!map.containsKey(chars)){
+            // was an if statement that checked if the element was already there or not, 
+            // not necessary because I made a string with exactly what I was looking for.
                 map.put(chars, map.getOrDefault(chars, 0) + 1);
-            }
+
+            //could do this individually
+
+
+            //old
             //map.put(chars,1);
             // am I doing this correctly? Should I be adding the same values with a count?
         }
@@ -24,21 +31,54 @@ public class MaxBalloons {
         //separate map for input
         HashMap<Character, Integer> map2 = new HashMap();
 
+        //old
+        //int ans = 0;
 
-        for (char chars: input){
+        for(char c : input){
+            map2.put(c, map2.getOrDefault(c, 0) +1);
+        
+        }
+
+        /**
+         * 
+         * 
+         * Old shit
+         * for (char chars : input){
             if(!map.containsKey(chars)){
                 map2.put(chars, map2.getOrDefault(chars, 0) + 1);
             }
+            // if(ans < map2.getOrDefault(chars, 0) + 1){
+            //     ans++;
+            // }
+
+        }*/
+
+        //this section was GPT
+
+        int maxBalloons = Integer.MAX_VALUE;
+        for(char chars : map.keySet()){
+            int required = map.get(chars);
+            int available = map2.getOrDefault(chars, 0);
+
+            maxBalloons = Math.min(maxBalloons, available/required);
+
+            //old
+            //if(map.get(chars).equals(map2.get(chars))){
+            //ans++;
+            //}
+            // if(map2.containsKey(chars)){
+            //     ans++;                
+            // }
+            //ans++;
         }
-
-        //compare two maps
-
-        int ans = 0;
-        
-
+        //old
         //ans = Collections.max(map2.values());
 
-        for (char key : map.keySet()){
+
+        
+         /* 
+         Old shit
+         for (char key : map.keySet()){
             if(map2.containsKey(key)){
                 ans++;
 
@@ -55,20 +95,21 @@ public class MaxBalloons {
             }
         }
 
-        // for (char chars:refArray){
-        //     for(int i = 0; i < theoreticalMax; i++){
-        //     if(map.containsKey(chars) && map2.containsKey(chars)){
-        //         ans = i;
-        //         }
-        //     }
-        // }
+        for (char chars:refArray){
+            for(int i = 0; i < theoreticalMax; i++){
+            if(map.containsKey(chars) && map2.containsKey(chars)){
+                ans = i;
+                }
+            }
+        }
 
-        //count number of balloons
+        count number of balloons */
+        
 
         
 
 
-        return ans;
+        return maxBalloons;
     }
 
     public static void main(String[] args){
@@ -81,10 +122,12 @@ public class MaxBalloons {
         String text4 = "poop";
         int output4 = 0;
 
-        System.out.println("Ans is: " + maxNumberOfBalloons(text) + ", expected " + output);
-        System.out.println("Ans is: " + maxNumberOfBalloons(text2) + ", expected " + output2);
-        System.out.println("Ans is: " + maxNumberOfBalloons(text3) + ", expected " + output3);
-        System.out.println("Ans is: " + maxNumberOfBalloons(text4) + ", expected " + output4);
+        MaxBalloons mb = new MaxBalloons();
+
+        System.out.println("Ans is: " + mb.maxNumberOfBalloons(text) + ", expected " + output);
+        System.out.println("Ans is: " + mb.maxNumberOfBalloons(text2) + ", expected " + output2);
+        System.out.println("Ans is: " + mb.maxNumberOfBalloons(text3) + ", expected " + output3);
+        System.out.println("Ans is: " + mb.maxNumberOfBalloons(text4) + ", expected " + output4);
     
     
     }
