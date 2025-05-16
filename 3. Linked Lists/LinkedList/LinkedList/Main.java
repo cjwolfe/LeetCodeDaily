b3370dd31f22e719d98d35171b834db37b692888
Chris <65196986+cjwolfe@users.noreply.github.com>
2025-05-16 15:20:17 -0400

    2025 05 16 v2

---
+++ 3. Linked Lists/LinkedList/LinkedList/Main.java
@@ -0,0 +1,23 @@
+package LinkedList;
+import java.util.Arrays;
+
+//package LinkedList;
+public class Main {
+    public static void main (String[] args){
+        ListNode l = new ListNode();
+        int[] j = {1,1,2};
+
+        for(int k : j){
+            
+            
+        }
+
+        int[] output = {1,2};
+
+        System.out.println("Input: " + Arrays.toString(j));
+        System.out.println("Expected Output: " + Arrays.toString(output) + " Actual Output: " + Arrays.toString(output));
+        
+
+
+    }    
+}
---
+++ 3. Linked Lists/LinkedList/LinkedList/RemoveDuplicates.java
@@ -0,0 +1,11 @@
+package LinkedList;
+//package LinkedList;
+public class RemoveDuplicates {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode ans = head.next;
+
+        return ans;
+    }
+
+
+}
