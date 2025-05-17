f56735dcedbfb84c3424d23d6a32b0d73878d959
Chris <65196986+cjwolfe@users.noreply.github.com>
2025-05-17 10:56:42 -0400

    2025 05 17 v1

--- 3. Linked Lists/LinkedList/LinkedList/Main.java
+++ 3. Linked Lists/LinkedList/LinkedList/Main.java
@@ -4,18 +4,28 @@ import java.util.Arrays;
 //package LinkedList;
 public class Main {
     public static void main (String[] args){
-        ListNode l = new ListNode();
+        //ListNode l = new ListNode();
         int[] j = {1,1,2};
+        
+        ListNode one = new ListNode(j[0]);
+        ListNode two = new ListNode(j[1]);
+        ListNode three = new ListNode(j[2]);
 
-        for(int k : j){
-            
-            
-        }
+        one.next = two;
+        two.next = three;
+        ListNode head = one;
+
+        RemoveDuplicates ans = new RemoveDuplicates();
+
+        ans.deleteDuplicates(head);
+
+        String answer = ans.toString();
+
 
         int[] output = {1,2};
 
         System.out.println("Input: " + Arrays.toString(j));
-        System.out.println("Expected Output: " + Arrays.toString(output) + " Actual Output: " + Arrays.toString(output));
+        System.out.println("Expected Output: " + Arrays.toString(output) + " Actual Output: " + answer);
         
 
 
