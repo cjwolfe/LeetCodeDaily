6c468dc5087d0ffbbd576949910fc166402483c9
Chris <65196986+cjwolfe@users.noreply.github.com>
2025-05-18 14:51:59 -0400

    Update RemoveDuplicates.java

--- 3. Linked Lists/LinkedList/LinkedList/RemoveDuplicates.java
+++ 3. Linked Lists/LinkedList/LinkedList/RemoveDuplicates.java
@@ -4,6 +4,14 @@ public class RemoveDuplicates {
     public ListNode deleteDuplicates(ListNode head) {
         ListNode ans = head.next;
 
+        // create temp variable
+        // iterate through list
+        // temp = current val
+        // if next value = temp
+        // delete next value
+        // go to next (while loop)
+
+
         return ans;
     }
 
