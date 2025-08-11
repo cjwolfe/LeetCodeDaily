da82a228ab648a4ef702245711606a7144b21c2d
Chris <65196986+cjwolfe@users.noreply.github.com>
2025-08-10 21:44:18 -0400

    2025 08 10 v3

--- Finances 3.0/Main.java
+++ Finances 3.0/Main.java
@@ -1,8 +1,31 @@
+// jfreechart import
+// swing gui import
 
 public class Main{
+
+    public void setup(){
+        //create wrappers, gui pieces, and a
+    }
+
+    public void promptName(){
+        // scan for name input, set to top of page, switch to main view?
+    }
+
+    public void setMonth(int i){
+        // this.month() = i;
+    }
+
+    public int getMonth(){
+    //return this.month();
+    return 0;
+    }
+
+    
 public static void main(String[] args){
     System.out.println("Hello World!");
-
+    Main ex = new Main();
+    ex.setup();
+    ex.promptName(); // gets the user's name
 
 
 }
