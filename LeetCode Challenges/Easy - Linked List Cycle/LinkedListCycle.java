e227ceb6b04ffeac25a3a51e757af1682b224c2d
Chris <65196986+cjwolfe@users.noreply.github.com>
2025-08-12 14:57:56 -0400

    2025 08 12

--- LeetCode Challenges/Easy - Linked List Cycle/LinkedListCycle.java
+++ LeetCode Challenges/Easy - Linked List Cycle/LinkedListCycle.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
  //Definition for singly-linked list.
 class ListNode {
@@ -7,13 +8,36 @@ class ListNode {
          val = x;
          next = null;
     }
- }
-
+}
 
 
 
 public class LinkedListCycle{
+
+    HashSet<Integer> set = new HashSet<>();
+
+
     public boolean hasCycle(ListNode head){
+        int pos = 0;
+        int tally = 0;
+        int inc = 0;
+        while (head != null){
+            pos = head.val;
+            System.out.println("pos " + pos);
+            if(set.contains(pos)){tally++;}
+            set.add(pos);
+            inc ++;
+
+                // Math.max(0,set.get(pos)+1)
+            System.out.println("Pos's count" + set.contains(pos)+1);
+            System.out.println("tally " + tally);
+            System.out.println("Increment: " + inc);
+            head = head.next;
+            //if(set.contains(pos)){return true;} 
+            if (tally>50){ return true;}
+        } 
+
+
         return false;
     }
 
@@ -31,8 +55,13 @@ public class LinkedListCycle{
         l2.next = l3;
         l3.next = l4;
         l4.next = l2;
-    
-    
+
+        LinkedListCycle ex = new LinkedListCycle();
+
+        System.out.println(ex.hasCycle(l1));
+
+
+
     }
     
 }
