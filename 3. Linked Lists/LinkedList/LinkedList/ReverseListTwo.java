0aa4635c2c47697c835f759d8ab91c80e8e8ec21
Chris <65196986+cjwolfe@users.noreply.github.com>
2025-08-16 09:30:18 -0400

    2025 08 16

--- 3. Linked Lists/LinkedList/LinkedList/ReverseListTwo.java
+++ 3. Linked Lists/LinkedList/LinkedList/ReverseListTwo.java
@@ -4,8 +4,20 @@ public class ReverseListTwo {
     
 
     public static void main(String[] args) {
-        System.out.println("Test");
+        
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);        
+        ListNode l3 = new ListNode(3);        
+        ListNode l4 = new ListNode(4);
+        ListNode l5 = new ListNode(5);
 
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        l4.next = l5;
+        l5.next = null;
+
+        
 
 
     }
